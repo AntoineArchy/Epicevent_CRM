b74f631ddcde9161e4104fa2d51872b93e0ae294
Archy <antoine.tassez@gmail.com>
2023-11-24 17:32:25 +0100

    Ajout d'une gestion des erreurs non gérée et d'un display/logging approprié

--- main_menu.py
+++ main_menu.py
@@ -1,20 +1,30 @@
-from SQL.connector import Connector
-from display.rich_cli_display import RichQuestionaryCliDisplay as Display
-from epic_event_CRM.base.context import Context
-from epic_event_CRM.controller import EpicEventController
+from base.connector import get_connect
+from base.context import Context
+from base.display.rich_cli_display import RichQuestionaryCliDisplay as Display
+from base.logger import logging
+from epic_event_CRM.main_controller import EpicEventController
 
-connector = Connector()
+connector = get_connect("epicevent")
 main_controller = EpicEventController()
 display = Display()
 context = Context(connector, display)
 
 run = True
 while run:
-    main_controller.update(context)
-    display.update(context)
+    try:
+        main_controller.update_state(context)
+        display.update(context)
 
-    if context.last_user_input == "Quit" or context.last_user_input is None:
-        run = False
-        break
+        if (
+            context.last_user_input == "Quit" or context.last_user_input is None
+        ):  # None est un ctrl+C interupt
+            run = False
+            break
 
-    main_controller.handle_user_input(context)
+        main_controller.handle_user_input(context)
+
+    except Exception as unhandled_error:
+        logging.error(f" Erreur non gérée : {str(unhandled_error)}")
+        error_message = "Une erreur non gérée s'est produite. Veuillez réessayer ou contacter le support."
+        display.show_error(error_message)
+        context.clear()  # On reset le context pour repartir sur le menu principal
